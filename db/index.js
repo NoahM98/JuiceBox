37b394cf0744abb3f49fca953344de9ad54da28b
Harshil Rama <harshilr98@gmail.com>
2023-06-28 19:32:45 -0500

    Created drop/create tables & functions

--- db/index.js
+++ db/index.js
@@ -1,7 +1,42 @@
 const { CLient, Client } = require('pg');
+const { clientVariable } = require('./pathVariable')
+const client = new Client({
+    host: "localhost",
+    port: 5432,
+    //database name is whatever was in psql, postgres is default
+    database: "juicebox-dev",
+    //also change username if you changed yours
+    user: "postgres",
+    password: "lakers1998"
+})
 
-const client = new Client('postgres://localhost:5432/juicebox-dev');
+async function getAllUsers() {
+    const { rows } = await client.query(
+        `SELECT id, username
+        FROM users; 
+    `);
+
+    return rows;
+
+}
+
+async function createUser({ username, password }) {
+    try {
+        const result = await client.query(`
+        INSERT INTO users (harshil, 1234)
+        VALUES ($1, $2);
+        `[username, password]);
+
+        return result;
+    } catch (error) {
+        throw error;
+    }
+}
+
 
 module.exports = {
     client,
+    getAllUsers,
+    createUser,
 }
+
--- db/seed.js
+++ db/seed.js
@@ -1,15 +1,61 @@
-const { client } = require('./index');
+const { client, getAllUsers, createUser } = require('./index');
+
+
 
 async function testDB() {
     try {
         client.connect();
-        const { rows } = await client.query(`SELECT * FROM users;`);
-        console.log(rows);
+        const users = await getAllUsers();
+        console.log(users);
     } catch (error) {
         console.error(error);
     } finally {
         client.end();
     }
 }
+
+async function dropTables() {
+    try {
+        await client.query(`
+        DROP TABLE IF EXISTS users; 
+        
+        `)
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function createTables() {
+    try {
+        await client.query(`
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            username varchar(255) UNIQUE NOT NULL,
+            password varchar(255) NOT NULL
+          );
+        
+        `)
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function rebuildDB() {
+    try {
+        client.connect();
+
+        await dropTables();
+        await createTables();
+
+    } catch (error) {
+        console.error(error);
+    } finally {
+        client.end();
+    }
+}
+
+// rebuildDB();
+
+
 
 testDB();
